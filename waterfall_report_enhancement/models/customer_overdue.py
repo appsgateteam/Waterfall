9b8a97d56bb462befe2fe3f5e789eb3b2bb9486a
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-08-11 16:34:16 +0400

    edit query in the payment report update

--- waterfall_report_enhancement/models/customer_overdue.py
+++ waterfall_report_enhancement/models/customer_overdue.py
@@ -23,21 +23,35 @@ class AccountInvoice(models.Model):
         self.env.cr.execute(query)
         match = self.env.cr.dictfetchall()
         for i in match:
-            overdue = self.env['account.invoice'].search(
-                [('date_due', '<=', current_date), ('state', '=', 'open'), ('user_id', '=', i['user_id']),
-                 ('type', 'in', ['out_invoice', 'out_refund'])])
+            # overdue = self.env['account.invoice'].search(
+            #     [('date_due', '<=', current_date), ('state', '=', 'open'), ('user_id', '=', i['user_id']),
+            #      ('type', 'in', ['out_invoice', 'out_refund'])])
+            querys = """SELECT ru.name as name ,
+                        ai.number as number,
+                        ai.origin as origin,
+                        ai.date_invoice as date_invoice,
+                        ai.date_due as date_due,
+                        ai.amount_total as amount_total,
+                        ai.residual as residual
+                        FROM ACCOUNT_INVOICE ai
+                        LEFT JOIN res_partner ru ON ru.id = ai.partner_id
+                        WHERE ai.date_due <= %s and ai.user_id = %s 
+                        AND ai.state='open' AND ai.type in ('out_invoice', 'out_refund')"""% (current_date,i['user_id'])
+            self.env.cr.execute(querys)
+            overdue = self.env.cr.dictfetchall()
+            
             
             for res in overdue:
                 overdue_content = {}
                 duecount = duecount + 1
                 overdue_content['inv_count'] = duecount
-                overdue_content['inv_no'] = res.number
-                overdue_content['origin'] = res.origin if res.origin else ''
-                overdue_content['customer'] = res.partner_id.name
-                overdue_content['inv_date'] = res.date_invoice
-                overdue_content['due_date'] = res.date_due
-                overdue_content['inv_amt'] = res.amount_total
-                overdue_content['due_amt'] = res.residual
+                overdue_content['inv_no'] = res['number']
+                overdue_content['origin'] = res['origin'] if res['origin'] else ''
+                overdue_content['customer'] = res['name']
+                overdue_content['inv_date'] = res['date_invoice']
+                overdue_content['due_date'] = res['date_due']
+                overdue_content['inv_amt'] = res['amount_total']
+                overdue_content['due_amt'] = res['residual']
                 overdue_list.append(overdue_content)
             data['invoice_overdue'] = overdue_list
             try:
