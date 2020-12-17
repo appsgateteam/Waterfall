ec05fec1d2432d1fafa21558e117d2785640fe9d
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-12-17 11:44:42 +0400

    edit in picking cancellation

--- qlty_cntrl/models/st_picking.py
+++ qlty_cntrl/models/st_picking.py
@@ -63,8 +63,8 @@ class StockPicking(models.Model):
     @api.multi
     def action_cancel(self):
         res = super(StockPicking, self).action_cancel()
-        self.sudo().mapped('check_ids').filtered(lambda x: x.quality_state == 'none').unlink()
-        self.update({'state':'cancel'})
+        # self.sudo().mapped('check_ids').filtered(lambda x: x.quality_state == 'none').unlink()
+        # self.update({'state':'cancel'})
         return res
 
     @api.multi
